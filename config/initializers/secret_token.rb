dcef6752e5ca37f3015abf15fd44af12dbccdf05
betten <betten@bc.edu>
2011-04-08 11:21:34 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lingo::Application.config.secret_token = 'd3f467c8ac9f0aee6ec5f243a312ec7a08d4a3bdbe145738aacc681dcf3da05be0f10337032bcf5b637454f5921952fb23e12e6b9daebea59cadfae2dbf5c492'
