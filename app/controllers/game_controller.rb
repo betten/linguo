bcaae2be5b750a04d035e5fdfb9cd541da8e6e75
betten <betten@bc.edu>
2011-05-28 11:33:18 -0400

    generated game controller, moved existing controllers into admin namespace

---
+++ app/controllers/game_controller.rb
@@ -0,0 +1,20 @@
+class GameController < ApplicationController
+  def home
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+end
---
+++ spec/views/game/home.html.haml_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe "game/home.html.haml" do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
