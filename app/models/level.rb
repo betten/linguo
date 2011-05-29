7f70b5462783a517a333140aa7fc304170b93900
betten <betten@bc.edu>
2011-05-29 13:00:11 -0400

    added levels language update_level_numbers call on after_save and after_destroy

--- app/models/level.rb
+++ app/models/level.rb
@@ -5,6 +5,14 @@ class Level < ActiveRecord::Base
   has_many :sentences
   has_many :vocabs
 
+  after_create do
+    self.language.update_level_numbers
+  end
+
+  after_destroy do
+    self.language.update_level_numbers
+  end
+
   def is_vocab?
     self.is_vocab
   end
@@ -13,4 +21,12 @@ class Level < ActiveRecord::Base
     self.vocabs
   end
 
+  include Comparable   
+  def <=> other
+    return 0 if !self.number && !other.number
+    return 1 if !self.number
+    return -1 if !other.number
+    self.number <=> other.number
+  end
+  
 end
--- spec/models/level_spec.rb
+++ spec/models/level_spec.rb
@@ -4,4 +4,22 @@ describe Level do
   it "has many sentences" do
     Level.new.should respond_to(:sentences)
   end
+
+  describe "level number" do
+    context "create" do
+      it "should be assigned a level number after create" do
+        language = Factory(:language)
+        language.should_receive(:update_level_numbers)
+        level = Level.create(:language => language)
+      end
+    end
+    context "destroy" do
+      it "should have the language reset its level numbers after destroy" do
+        language = Factory(:language)
+        language.should_receive(:update_level_numbers).twice
+        level = Level.create(:language => language)
+        level.destroy
+      end
+    end
+  end
 end
