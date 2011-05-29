45823a948dd3e6703b5a8eaf4d715bac85902287
betten <betten@bc.edu>
2011-05-29 12:59:31 -0400

    specd and added update_level_numbers method to language model

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,4 +1,12 @@
 class Language < ActiveRecord::Base
   has_many :games
   has_many :levels
+
+  def update_level_numbers
+    self.levels.sort.each_with_index do |level, index|
+      level.number = index + 1
+      level.save
+    end
+  end
+
 end
--- spec/models/language_spec.rb
+++ spec/models/language_spec.rb
@@ -1,5 +1,66 @@
 require 'spec_helper'
 
 describe Language do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "update_level_numbers" do
+
+    before do
+      @language = Factory(:language)
+    end
+
+    context "one or more of the languages levels has a nil number" do
+      before do
+        @language.levels << Factory(:level, :number => 5)
+        @language.levels << Factory(:level)
+        @language.levels << Factory(:level, :number => 2)
+        @language.levels << Factory(:level)
+        @language.levels << Factory(:level, :number => 8)
+      end
+
+      it "should set level numbers for all levels" do
+        @language.update_level_numbers
+        @language.levels.map(&:number).should_not include(nil)
+      end
+
+      it "should result in sorted 1..n level numbers" do
+        @language.update_level_numbers
+        @language.levels.sort.map(&:number).should == (1..@language.levels.count).to_a
+      end
+    end
+
+    context "language level numbers are unsorted" do
+      before do
+        (1..10).to_a.sort_by{ rand }.each do |n|
+          @language.levels << Factory(:level, :number => n)
+        end
+      end
+
+      it "should set level numbers for all levels" do
+        @language.update_level_numbers
+        @language.levels.map(&:number).should_not include(nil)
+      end
+
+      it "should result in sorted 1..n level numbers" do
+        @language.update_level_numbers
+        @language.levels.sort.map(&:number).should == (1..@language.levels.count).to_a
+      end
+    end
+
+    context "language level numbers are already sorted 1..n" do
+      before do
+        (1..10).to_a.each do |n|
+          @language.levels << Factory(:level, :number => n)
+        end
+      end
+
+      it "should set level numbers for all levels" do
+        @language.update_level_numbers
+        @language.levels.map(&:number).should_not include(nil)
+      end
+
+      it "should result in sorted 1..n level numbers" do
+        @language.update_level_numbers
+        @language.levels.sort.map(&:number).should == (1..@language.levels.count).to_a
+      end
+    end
+  end
 end
