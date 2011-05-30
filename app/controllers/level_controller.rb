6ecf047d7d8a5cadb4c35723b900c2a46eb33f17
betten <betten@bc.edu>
2011-05-29 23:43:29 -0400

    implemented level show action and views

--- app/controllers/level_controller.rb
+++ app/controllers/level_controller.rb
@@ -1,5 +1,9 @@
 class LevelController < ApplicationController
+  before_filter :authenticate_user!
+
   def show
+    @game = Game.find(params[:game_id])
+    @level = Level.find(params[:id])
   end
 
   def challenge
