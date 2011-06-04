10d01ee04f52cd21251a5c6d3d8727d7f7bfefe7
betten <betten@bc.edu>
2011-06-03 22:52:34 -0400

    implemented challenge page base ui and formatting

--- app/controllers/level_controller.rb
+++ app/controllers/level_controller.rb
@@ -4,9 +4,20 @@ class LevelController < ApplicationController
   def show
     @game = Game.find(params[:game_id])
     @level = Level.find(params[:id])
+    authorize! :show, @level
   end
 
   def challenge
+    @game = Game.find(params[:game_id])
+    @level = Level.find(params[:id])
+
+    if @level.is_vocab?
+      @testing = @level.vocab
+    else
+      @testing = @level.sentences
+    end
+
+    authorize! :challenge, Level
   end
 
 end
