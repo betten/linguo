604881ff39e99ff88771a3ca71faf5a442d482d1
betten <betten@bc.edu>
2011-06-15 22:21:40 -0400

    added enabled scope to language model and enabled? method

--- app/models/language.rb
+++ app/models/language.rb
@@ -2,6 +2,12 @@ class Language < ActiveRecord::Base
   has_many :games
   has_many :levels
 
+  scope :enabled, where("enabled = ?", true)
+
+  def enabled?
+    self.enabled
+  end
+
   def update_level_numbers
     self.levels.sort.each_with_index do |level, index|
       level.number = index + 1
